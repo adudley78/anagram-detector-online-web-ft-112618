f8a172316556ea29a0cefa04aa378f40d3405cda
Adam Dudley <dudleyadam@gmail.com>
2018-12-07 16:27:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  #attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    #binding.pry
+    #needs to detect no matches
+    array.select do | w |
+      anagram_test(w)
+  end
+
+  def anagram_test(w)
+    word.chars.sort == @word.chars.sort
+  end
+
+end
